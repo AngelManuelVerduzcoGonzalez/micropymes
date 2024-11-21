ac38a5e438d2a9cbbd8313f7c604f90fe298fb96
AngelMVG <verduzcogonzalezangelmanuel3@gmail.com>
2024-11-21 17:42:00 -0600

    Created interface and logic for home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
+  goToInventario() {
+    this.router.navigate(['/productos']);
+  }
+  goToVentas() {
+    this.router.navigate(['/ventas']);
+  }
+  goToClientes() {
+    this.router.navigate(['/clientes']);
+  }
+  goToReportes() {
+    this.router.navigate(['/reportes']);
+  }
+  goToCompras() {
+    this.router.navigate(['/compras']);
+  }
+  goToProveedores() {
+    this.router.navigate(['/proveedores']);
+  }
+  goToAjustes() {
+    this.router.navigate(['/ajustes']);
+  }
+  logout() {
+    this.authService.logout();
+  }
 }
